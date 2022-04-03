655ff1e4780875a7ed5abb4abfa2d912856ea8d6
tt-adisoh <ktx030430@naver.com>
2022-04-03 23:45:10 +0900

    20220403

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,23 @@
+# Code by 2022094093 Dohoon Kim, Dept. of Data Science.
 import random
 
 word_list = ["hanyang university", "computer science", "data science", "python programming"]
 
+# Get user's name, and show Intro.
 def get_user_name():
     name = input("What's your name?\n: ")
-    print(f"Good Luck, {name}!")
+    print(f"Good, {name}!")
     print("We are going to play a guess the word game.")
+    print("You have 10 chances to guess the word.")
+    print("Words are about our school.")
     print("The word will be chosen by the computer.\n")
 
+# Randomly choose word from 'word_list'
 def get_random_word(list):
     return random.choice(list)
 
+# Make answer word to underbar.
+# For example, "apple" changes to "_____".
 def make_underbar_word(word):
     hidden_word = ""
     for i in range(len(word)):
@@ -31,15 +38,17 @@ def guessing(answer_word):
             print(underbar_word)
             alphabet = input("Guess : ")
             if len(alphabet) == 1:
-                for k in range(len(answer_word)):
+                for k in range(len(answer_word)):  # Underbar to alphabet letter
                     if alphabet == answer_word[k] and underbar_word[k] == "_":
                         underbar_word = underbar_word[:k] + alphabet + underbar_word[k+1:]
                         now_guess += 1
                 print(f"You guessed {now_guess}.")
+            elif alphabet == answer_word:  # When more than one letter entered, and answer is correct
+                print(f"Congratulations!! You finally guessed the word '{answer_word}'!!")
+                need_to_try = False
             else:
-                if alphabet in answer_word:
-                    slice_index = answer_word.find(alphabet)
-                    underbar_word = underbar_word[:slice_index] + alphabet + underbar_word[slice_index+len(alphabet):]
+                print("Nope! I think you tried to guess the full word,")
+                print(f"But '{alphabet}' is not the answer.")
             chances -= 1
 
             if chances > 0:
@@ -56,6 +65,7 @@ def guessing(answer_word):
         print(f"The answer was '{answer_word}'.")
         print("Game Over.")
 
+# Start Guessing Game !!
 get_user_name()
 answer = get_random_word(word_list)
 target_sentence = make_underbar_word(answer)
