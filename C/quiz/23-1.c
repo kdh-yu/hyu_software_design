e7a5051a1bdce39636a875e85607f15295d3ae7b
tt-adisoh <ktx030430@naver.com>
2022-06-10 13:32:18 +0900

    algorithm

--- C/quiz/23-1.c
+++ C/quiz/23-1.c
@@ -6,7 +6,6 @@ Store the input integers into a 4x4 array. Print the sum of each column in the a
 
 int main(){
     int array[4][4];
-    int column_sum[4] = {0, 0, 0, 0};
     
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
@@ -15,13 +14,11 @@ int main(){
     }
 
     for (int i = 0; i < 4; i++){
+        int column_sum = 0;
         for (int j = 0; j < 4; j++){
-            column_sum[i] += array[j][i];
+            column_sum += array[j][i];
         }
-    }
-    
-    for (int len = 0; len < 4; len++){
-        printf("%d ", column_sum[len]);
+        printf("%d ", column_sum);
     }
     printf("\n");
     return 0;
